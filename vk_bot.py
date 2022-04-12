ab6fae73ca8c774ac9cd23c7e4643af383acf14f
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-04-12 20:43:07 +0300

    VK bot don't respond to an unclear message

--- vk_bot.py
+++ vk_bot.py
@@ -3,10 +3,25 @@ import random
 import uuid
 
 import vk_api as vk
+from google.cloud import dialogflow
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-from bot import detect_intent_texts
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    if not response.query_result.intent.is_fallback:
+        return response.query_result.fulfillment_text
 
 
 def echo(event, vk_api):
@@ -16,7 +31,8 @@ def echo(event, vk_api):
         event.text,
         language_code='ru'
     )
-    
+    if not text:
+        return
     vk_api.messages.send(
         user_id=event.user_id,
         message=text,
